5ff9908044712e7b5b2e56b49c4a34b455b2d690
Mariya Lubyankina <marigutova2009@ya.ru>
2024-02-27 23:03:50 +0300

    ДЗ по 14 уроку

---
+++ src/main/java/ru/otus/java/basic/homeworks/lesson14/AppArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks.lesson14;
+
+public class AppArrayDataException  extends RuntimeException{
+    public AppArrayDataException (String message){
+        super(message);
+    }
+}
